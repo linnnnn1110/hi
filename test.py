221dee1e3abef58c717af357d3f8dc8b90af1aca
吳羚綾 <lingwu_aurora@outlook.com>
2023-07-18 17:44:16 +0800

    new test

---
+++ test.py
@@ -0,0 +1,15 @@
+x = 5
+
+price = 10
+pen = 0.38
+rain = True
+name = 'Aurora'
+
+
+print(price, pen, rain, name)
+
+
+name1 = input('請輸入名字:')
+
+print(price, pen, rain, name)
+print('Hello', name1)
